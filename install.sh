aa1e363495bf2cca51423ae24cad0fa1a0eaea77
Mohsen Hosseini <61708474+mhsnhsn@users.noreply.github.com>
2024-12-11 00:06:06 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://github.com/mhsnhsn/customized/blob/main/config.json
+
 red='\033[0;31m'
 green='\033[0;32m'
 yellow='\033[0;33m'
@@ -231,4 +231,3 @@ install_s-ui() {
 echo -e "${green}Executing...${plain}"
 install_base
 install_s-ui $1
-cp config.json /usr/local/s-ui/bin/
